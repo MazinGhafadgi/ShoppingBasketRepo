2103f01639217254c031776a5c4b2a0106caebc7
Mazin Ghafadgi <mazin.ghafadgi@vodafone.com>
2020-03-17 09:12:50 +0000

    Refactor the code and print (No offers available) and removed unused object

--- src/main/scala/uk/adthena/shoppingbasket/ShoppingBasket.scala
+++ src/main/scala/uk/adthena/shoppingbasket/ShoppingBasket.scala
@@ -13,7 +13,7 @@ case class Item(itemType: ItemType, price: BigDecimal)
 
 trait ShoppingBasket {
 
-  private var items: List[Item] = List.empty
+   protected var items: List[Item] = List.empty
 
   def addToBasket(args: Array[String]): Unit = {
     args.foreach{
@@ -29,12 +29,20 @@ trait ShoppingBasket {
 
   private def add(item: Item) = items = item :: items
 
-  def calculateAppleItems(groupedItems: List[Item]) = groupedItems.foldLeft(BigDecimal(0)) { (acc, item) => acc + item.price }
-  def calculateBreadItems(groupedItems: List[Item], soups: List[Item]) = groupedItems.foldLeft(BigDecimal(0)) { (acc, item) => acc + item.price }
+  protected def totalPrice() = items.foldLeft(BigDecimal(0)) { (acc, item) => acc + item.price }
 
-  def checkout() = {
-    println(s"Subtotal: £${"%.2f".format(items.foldLeft(BigDecimal(0)) { (acc, item) => acc + item.price })}")
+  def checkout(): BigDecimal = {
+    println(s"Subtotal: £${"%.2f".format(totalPrice())}")
+    println("(No offers available)")
+    totalPrice
+  }
+}
 
+trait DiscountOnPrice {
+  self: ShoppingBasket =>
+
+  override def checkout(): BigDecimal = {
+    println(s"Subtotal: £${"%.2f".format(totalPrice())}")
     items.groupBy(_.itemType).map { case (key, groupedItems) => key match {
       //Apple
       case ItemType.Apple => calculateAppleItems(groupedItems)
@@ -47,17 +55,14 @@ trait ShoppingBasket {
     }
     }.foldLeft(BigDecimal(0)) { (acc, item) => acc + item }
   }
-}
 
-trait DiscountOnPrice {
-  self: ShoppingBasket =>
-   override def  calculateAppleItems(groupedItems: List[Item]) = {
+   private def  calculateAppleItems(groupedItems: List[Item]) = {
     val total = groupedItems.foldLeft(BigDecimal(0)) { (acc, item) => acc + item.price }
       println("Apples 10% off: 10p")
       total - (total * .10)
   }
 
-  override def calculateBreadItems(breadItem: List[Item], soups: List[Item]) = {
+  private def calculateBreadItems(breadItem: List[Item], soups: List[Item]) = {
     println(s"(total bread items ${breadItem.length}, applying half price on ${soups.length / 2} bread items)")
     val numberOfSoups: Int = if(soups.nonEmpty) soups.length / 2 else 0
     breadItem.foldLeft((numberOfSoups, BigDecimal(0))) { (acc, item) => if (acc._1 != 0) (acc._1 - 1, acc._2 + (item.price / 2)) else (0, acc._2 + item.price)
